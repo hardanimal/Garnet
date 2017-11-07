8989228047a4dab6699dc76329a50ca4fdc2635d
danzel.li <hardanimal@hotmail.com>
2017-11-07 16:22:34 +0800

    Update the main code to start 4 "Channel' thread that can conduct test simultaneously for 4 Erie boards.

--- src/UFT_GUI/main.py
+++ src/UFT_GUI/main.py
@@ -126,34 +126,90 @@ class Update(QtCore.QThread):
         self.capacitor_barcodes_4 = capacitor_barcodes_4
         self.mode = mode
 
+    def getcurrentprocessbar(self, f_ch1, bar1,
+                             f_ch2, bar2,
+                             f_ch3, bar3,
+                             f_ch4, bar4):
+        if not f_ch1:
+            bar1 = 100
+        if not f_ch2:
+            bar2 = 100
+        if not f_ch3:
+            bar3 = 100
+        if not f_ch4:
+            bar4 = 100
+        return min(bar1, bar2, bar3, bar4)
+
+
     def run(self):
         sec_count = 0
-        ch = Channel(barcode_list=self.barcodes_1, cable_barcodes_list=self.cable_barcodes_1, capacitor_barcodes_list=self.capacitor_barcodes_1,
+        ch1 = Channel(barcode_list=self.barcodes_1, cable_barcodes_list=self.cable_barcodes_1, capacitor_barcodes_list=self.capacitor_barcodes_1,
                           channel_id=0, name="UFT_CHANNEL", mode4in1=self.mode)
-        #ch.setDaemon(True)
-        ch.auto_test()
+        ch2 = Channel(barcode_list=self.barcodes_2, cable_barcodes_list=self.cable_barcodes_2, capacitor_barcodes_list=self.capacitor_barcodes_2,
+                          channel_id=1, name="UFT_CHANNEL", mode4in1=self.mode)
+        ch3 = Channel(barcode_list=self.barcodes_3, cable_barcodes_list=self.cable_barcodes_3, capacitor_barcodes_list=self.capacitor_barcodes_3,
+                          channel_id=2, name="UFT_CHANNEL", mode4in1=self.mode)
+        ch4 = Channel(barcode_list=self.barcodes_4, cable_barcodes_list=self.cable_barcodes_4, capacitor_barcodes_list=self.capacitor_barcodes_4,
+                          channel_id=3, name="UFT_CHANNEL", mode4in1=self.mode)
+        ch1.auto_test()
+        ch2.auto_test()
+        ch3.auto_test()
+        ch4.auto_test()
         self.emit(QtCore.SIGNAL("is_alive"), 1)
-        while ch.isAlive():
+        while ch1.isAlive() or ch2.isAlive() or ch3.isAlive() or ch4.isAlive():
             sec_count += 1
-            self.emit(QtCore.SIGNAL("progress_bar"), ch.progressbar)
+            c_process = self.getcurrentprocessbar(ch1.isAlive(), ch1.progressbar,
+                                                  ch2.isAlive(), ch2.progressbar,
+                                                  ch3.isAlive(), ch3.progressbar,
+                                                  ch4.isAlive(), ch4.progressbar)
+            self.emit(QtCore.SIGNAL("progress_bar"), c_process)
             self.emit(QtCore.SIGNAL("time_used"), sec_count)
-            for dut in ch.dut_list:
+            for dut in ch1.dut_list:
                 if dut is not None:
                     self.emit(QtCore.SIGNAL("dut_status_1"), dut.slotnum,
                               dut.status)
+            for dut in ch2.dut_list:
+                if dut is not None:
+                    self.emit(QtCore.SIGNAL("dut_status_2"), dut.slotnum,
+                              dut.status)
+            for dut in ch3.dut_list:
+                if dut is not None:
+                    self.emit(QtCore.SIGNAL("dut_status_3"), dut.slotnum,
+                              dut.status)
+            for dut in ch4.dut_list:
+                if dut is not None:
+                    self.emit(QtCore.SIGNAL("dut_status_4"), dut.slotnum,
+                              dut.status)
             time.sleep(1)
 
-        self.emit(QtCore.SIGNAL("progress_bar"), ch.progressbar)
-        for dut in ch.dut_list:
+        self.emit(QtCore.SIGNAL("progress_bar"), 100)
+        for dut in ch1.dut_list:
             if dut is not None:
                 self.emit(QtCore.SIGNAL("dut_status_1"), dut.slotnum, dut.status)
+        for dut in ch2.dut_list:
+            if dut is not None:
+                self.emit(QtCore.SIGNAL("dut_status_2"), dut.slotnum, dut.status)
+        for dut in ch3.dut_list:
+            if dut is not None:
+                self.emit(QtCore.SIGNAL("dut_status_3"), dut.slotnum, dut.status)
+        for dut in ch4.dut_list:
+            if dut is not None:
+                self.emit(QtCore.SIGNAL("dut_status_4"), dut.slotnum, dut.status)
 
-        ch.save_db()
-
-        time.sleep(1)
-
-        if ch is not None:
-            del ch
+        # clean resource
+        if ch1 is not None:
+            ch1.save_db()
+            time.sleep(0.5)
+            del ch1
+        if ch2 is not None:
+            ch2.save_db()
+            del ch2
+        if ch3 is not None:
+            ch3.save_db()
+            del ch3
+        if ch4 is not None:
+            ch4.save_db()
+            del ch4
         #self.terminate()
         self.emit(QtCore.SIGNAL("is_alive"), 0)
 

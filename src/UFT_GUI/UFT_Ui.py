76eb8051e64bff0fcec82fc79e0637f1de47df6c
danzel.li <hardanimal@hotmail.com>
2017-12-19 10:12:24 +0800

    Update to version 5.4.5.

--- src/UFT_GUI/UFT_Ui.py
+++ src/UFT_GUI/UFT_Ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'UFT_Ui.ui'
 #
-# Created: Mon Dec 18 14:15:43 2017
+# Created: Tue Dec 19 10:12:00 2017
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -2529,7 +2529,7 @@ class Ui_Form(object):
         self.search_lineEdit.setPlaceholderText(_translate("Form", "search by serial number", None))
         self.search_pushButton.setText(_translate("Form", "Search", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Form", "test log", None))
-        self.title_label.setText(_translate("Form", "AGIGA UPGEM UFT V5.4.4", None))
+        self.title_label.setText(_translate("Form", "AGIGA UPGEM UFT V5.4.5", None))
         self.start_pushButton.setText(_translate("Form", "START", None))
         self.Mode4in1.setText(_translate("Form", "4 in 1", None))
 
